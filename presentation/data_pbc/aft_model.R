9147b23555341f1bb0e26075e43f84ceceb02574
Areiche <reichear@hu-berlin.de>
2018-02-01 12:19:32 +0100

    Adjusted atf_model.R to be final

--- presentation/data_pbc/aft_model.R
+++ presentation/data_pbc/aft_model.R
@@ -21,24 +21,6 @@ summary(atf_model_exponential)
 atf_model_log <- survreg(Surv(pbc$time, pbc$status == 2) ~ pbc$age + pbc$edema + log(pbc$bili) + log(pbc$albumin) + log(pbc$protime), dist='loglogistic')
 summary(atf_model_log)
 
-#Usage of SurvRegCensCov for Weibull ATF
-library(SurvRegCensCov)
-data(larynx)
-WeibullReg(Surv(time, death) ~ factor(stage) + age, data=larynx)
-WeibullDiag(Surv(time, death) ~ factor(stage), data = larynx,
-labels=c("Stage I", "Stage II", "Stage III", "Stage IV"))
-
-##Compare output with s1 summary. The differences is that WeibullReg contains already the hazard ratios
-s3 <- WeibullReg(Surv(futime, fustat)~1  , ovarian)
-s3
-
-##Produce the diagnostic Plot, plots log Time versus the log of the estimated cumulative hazard estimate.
-##If the Weibull model has adequate fit, then the plots for each of the covariates should be roughly linear
-##and parallel.
-WeibullDiag(Surv(futime, fustat)~factor(rx) , ovarian,
-labels=c("Stage I", "Stage II"))
-
-
 ##Receive AUC and ROC plot for all ATF models
 par(mfrow = c(2, 2))
 pred_atf <- prediction(predict(atf_model_weibull), pbc$status)
